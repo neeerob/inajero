6b7e724888bbe7461c106dc0da58aebae2c255c4
Nirob Ahmed <neeerob.ahmed@gmail.com>
2023-02-05 23:22:46 +0600

    Lab Task-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ModeratorModule } from './Moderator/moderator.module';
+
 
 @Module({
-  imports: [],
+  imports: [ModeratorModule],
   controllers: [AppController],
   providers: [AppService],
 })
